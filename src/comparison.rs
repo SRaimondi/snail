52756fe66cd1ca2cc8a930d9b21420c08069fe7f
Simone Raimondi <simo@cleverit.ch>
2021-11-27 16:40:20 +0100

    Added method to compare directly for zero if that's what you want

--- src/comparison.rs
+++ src/comparison.rs
@@ -22,6 +22,13 @@ pub trait ApproxEq: Sized {
             Self::DEFAULT_ABSOLUTE_EPS,
         )
     }
+
+    fn approx_zero_eps(self, eps: Self) -> bool;
+
+    #[inline(always)]
+    fn approx_zero(self) -> bool {
+        self.approx_zero_eps(Self::DEFAULT_ABSOLUTE_EPS)
+    }
 }
 
 macro_rules! generate_approx_eq {
@@ -30,6 +37,7 @@ macro_rules! generate_approx_eq {
             const DEFAULT_RELATIVE_EPS: Self = $def_rel_eps;
             const DEFAULT_ABSOLUTE_EPS: Self = $def_abs_eps;
 
+            #[inline(always)]
             fn approx_eq_eps(self, other: Self, rel_eps: Self, abs_eps: Self) -> bool {
                 let diff_abs = (self - other).abs();
                 if diff_abs <= abs_eps {
@@ -39,6 +47,11 @@ macro_rules! generate_approx_eq {
                 let other_abs = other.abs();
                 let largest = self_abs.max(other_abs);
                 diff_abs <= largest * rel_eps
+            }
+
+            #[inline(always)]
+            fn approx_zero_eps(self, eps: Self) -> bool {
+                self.abs() <= eps
             }
         }
     };
